1a0bd67f9c52e3ea8d88ff8b750cabcfa798e591
Julian Beck <julian16.beck@hotmail.com>
2024-12-09 19:14:57 +0100

    Second part of day1

--- 1/chiefHistorian.py
+++ 1/chiefHistorian.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 data = pd.read_csv('data.tsv', sep="\t")
@@ -9,4 +10,8 @@ df = pd.DataFrame()
 df['col1'] = firstList
 df['col2'] = secondList
 df['result'] = df['col1'] - df['col2']
+df['countLeft'] = [np.count_nonzero(secondList == x) for x in firstList]
+df['result2'] = df['col1'] * df['countLeft']
 print(df['result'].abs().sum())
+
+print(df['result2'].sum())
